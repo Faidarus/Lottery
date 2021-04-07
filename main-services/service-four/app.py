2231e5ae0026b494863648cc5b89d166df25b571
Faidarus <fatima_aidarus@hotmail.co.uk>
2021-04-07 16:29:16 +0000

    edited files

--- main-services/service-four/app.py
+++ main-services/service-four/app.py
@@ -9,16 +9,29 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 db = SQLAlchemy(app)
 
 class Lottery_form(FlaskForm):
-    third_name = StringField('Name')
+    name = StringField('Name')
     submit = SubmitField('Add Name')
 
 
 @app.route('/lottery',methods=['GET'])
 def winner():
-
+combined_lists = ""
+prize = 250
+number_choice = [1, 3, 5, 7, 9]
 service-two = requests.get('http://localhost:5002/numbers').text
 service-three = requests.get('http://localhost:5003/letters').text
+combined_lists = numbers + letters
+random.choice(number_choice) 
+for f in range (len(combined_lists)):
+    if random.choice(number_choice) = 5 in combined_lists:
+        won = prize
+        
 
+
+
+db.create_all()
+db.session.add(winner)
+db.session.commit()
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True, host='0.0.0.0')
--- main-services/service-one/app.py
+++ main-services/service-one/app.py
@@ -2,10 +2,11 @@ from flask import Flask, Response, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/',methods=['GET', 'POST'])
 def home():
-
-service-four = requests.get('http://localhost:5001')
+service-two = requests.get('http://localhost:5002/numbers').text
+service-three = requests.get('http://localhost:5003/letters').text
+service-four = requests.post('http://localhost:5001/winner').text
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host='0.0.0.0')
