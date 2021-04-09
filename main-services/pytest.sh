b0428f4d7efd108e7ecd70def3973136992da292
Faidarus <fatima_aidarus@hotmail.co.uk>
2021-04-09 15:49:29 +0000

    edited jenkinsfile

--- main-services/pytest.sh
+++ main-services/pytest.sh
@@ -3,9 +3,10 @@ sudo apt-get install python3-venv -y
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+pip3 install pytest
+pip3 install pytest-cov
 
-pytest
-pytest --cov=app
-pytest --cov-config=.coveragec --cov=.
-pytest --cov=app --cov-report=term-missing
-pytest --cov . --cov-report html
+pytest service_one --cov --cov-report=term-missing 
+pytest service_two --cov --cov-report=term-missing 
+pytest service_three --cov --cov-report=term-missing 
+pytest service_four --cov --cov-report=term-missing 
