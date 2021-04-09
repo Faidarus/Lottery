04605b8b301746f35edbfae5f1f2f7b540f39fba
Faidarus <fatima_aidarus@hotmail.co.uk>
2021-04-09 15:10:49 +0000

    final

--- main-services/service_three/testing/test_app.py
+++ main-services/service_three/testing/test_app.py
@@ -1,15 +1,23 @@
+from app import app
 from unittest.mock import patch
+from flask import url_for
 from flask_testing import TestCase
+import random
+import string
 
 class TestBase(TestCase):
-    def create_app(self)
-    return app
+    def create_app(self):
+        return app
 
 class TestLetters(TestBase):
     def test_letters(self):
-        response = self.client.data(url("letters"))
-        self.assert(len(response.text), L)
+        response = self.client.get(url_for('letters'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_letters(self):         
+        with patch('random.choice') as a:
+            a.return_value = 'A'
+            response = self.client.get(url_for('letters'))
+            self.assertIn(b'AAA', response.data)
         
-          
-with patch('empty_letter_list') as a:
-    a.return_value = ['DYJJOVLW']
+  
