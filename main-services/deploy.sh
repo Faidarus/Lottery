bb15e5f34d8463693ca79f853998fc020ca21b6a
Faidarus <fatima_aidarus@hotmail.co.uk>
2021-04-09 18:54:29 +0000

    editing files

--- main-services/deploy.sh
+++ main-services/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@swarm-master:docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-master << EOF
     export DATABASE_URI=${DATABASE_URI}
-    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml main-services
+    docker stack deploy --compose-file docker-compose.yaml main-services
 EOF
