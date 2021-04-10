b3908b4a111d969478e61950c3ddd7bf4a3dfefc
Faidarus <fatima_aidarus@hotmail.co.uk>
2021-04-10 09:39:16 +0000

    edited the nginx

--- main-services/service_one/testing/test_app.py
+++ main-services/service_one/testing/test_app.py
@@ -2,8 +2,32 @@ from app import app
 from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
+from app import db
+from application import db, 
 import requests_mock
 
+# pytest
+# pytest --cov=app
+# pytest --cov-config=.coveragec --cov=.
+# pytest --cov=app --cov-report=term-missing
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
+                DEBUG=True,
+        return app    
+
+    def setUp(self):
+        
+        db.create_all()
+
+        test_lottery = Lottery(combined_account_string="234567JJK", message_string="Congratulation you won £125", lottery=1)
+        db.session.add(test_lottery)
+        db.session.commit()
+    
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 class TestBase(TestCase):
     def create_app(self):
@@ -14,10 +38,17 @@ class TestHome(TestBase):
         with requests_mock.mock() as m:
             m.get("http://service_two:5002/numbers").text = ('3')
             m.get("http://service_two:5003/letters").text = ("A")
-            # m.post("http://service_four:5001/lottery").json = ("new_winner")
             response = self.client.get(url_for('home'))
-            # self.assertIn(b'A', response.data)
 
-    def test_home(self):
-        response = self.client.get(url_for('home'))
-        self.assertEqual(response.status_code, 500)
+    # def test_view(self):
+    #     response = self.client.get(url_for('home'))
+    #     self.assertIsNotNone(response.status_code, 200)
+    #     self.assertIsNotNone(response.data)
+    
+    # def test_home_combined(self):
+    #     with patch('requests.get') as f:
+    #         f.return_value.text = "123456CAB"
+    #         response = self.client.get(url_for('home'))
+    #         self.assertIsNotNon(response.status_code, 200)
+    #         self.assertIsNotNone(response.data)
+
