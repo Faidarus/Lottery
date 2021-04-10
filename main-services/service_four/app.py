1643297dab3a8355109fe7a487362bc78fb93bbb
Faidarus <fatima_aidarus@hotmail.co.uk>
2021-04-10 15:36:02 +0000

    made all the service changes to app.py and edited some issues I had with my other application as well such as requirement.txt, delpy.sh ect

--- main-services/service_four/app.py
+++ main-services/service_four/app.py
@@ -5,13 +5,13 @@ import string
 app = Flask(__name__)
 @app.route('/lottery',methods=['POST'])
 def winner():
-    prize = 250 #predefined variable
+    prize = 500 #predefined variable
     won = 0
     message = f"You didn't win £{won}"
-    number_choice = ["1","3","5","7","9"]
+    number_choice = ["2","4","6","8","10"]
     data = request.get_json()
     combined_lists = data["service_numbers"] + data["service_letters"] #combined services into one variable
-    if random.choice(number_choice) == combined_lists[3] or random.choice(number_choice) == combined_lists[5]: #Check to see if there is a random number in specific index value
+    if random.choice(number_choice) == combined_lists[2] or random.choice(number_choice) == combined_lists[4]: #Check to see if there is a random number in specific index value
         won += prize/2  #adds half the prize to the current winnings
         message = f"Congratulation you won £{won}" #change the message
     return jsonify({"combined":combined_lists,"message":message})
--- main-services/service_four/testing/test_app.py
+++ main-services/service_four/testing/test_app.py
@@ -3,8 +3,16 @@ from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
 import requests_mock
+import requests
 import random
 import string
+
+# pytest
+# pytest --cov=app
+# pytest --cov-config=.coveragec --cov=.
+# pytest --cov=app --cov-report=term-missing
+
+
 
 
 class TestBase(TestCase):
@@ -14,14 +22,19 @@ class TestBase(TestCase):
 class TestLottery(TestBase):
     def test_lottery(self):
         with requests_mock.mock() as m:
-            m.get("http://service_two:5002/numbers").text = ('3')
-            m.get("http://service_two:5003/letters").text = ("A")
-            m.post("http://service_four:5001/lottery"),json = ("combined_list")
-            response = self.client.get(url_for('winner')
-    # def test_winner(self):
-    #     response = self.client.post(url_for('winner'))
-    #     self.assertEqual(response.status_code, 500)
-       
-   
+            m.get("http://service_two:5002/numbers").text = ('2')
+            m.get("http://service_two:5003/letters").text = ("a")
+            response = self.client.get(url_for('winner'))
     
-  
+    
+    def test_winner(self):
+        response = self.client.post(url_for('winner'))
+        self.assertIsNotNone(response.status_code, 200)
+        self.assertIsNotNone(response.data)
+   
+    # def test_winner(self):
+    #     with patch('requests.get') as f:
+    #         f.return_value.text ="C"
+    #         response = self.client.post(url_for('winner'))
+    #         self.assertEqual(response.status_code, 200)
+    #         self.assertIsNotNone(response.data)
