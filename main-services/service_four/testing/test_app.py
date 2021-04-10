7d021ba18f3743c5dbb3e2f6e81f1c716599a94e
Faidarus <fatima_aidarus@hotmail.co.uk>
2021-04-10 14:57:38 +0000

    testing my tests on jenkinsfile pipeline

--- main-services/service_four/testing/test_app.py
+++ main-services/service_four/testing/test_app.py
@@ -10,6 +10,7 @@ import string
 # pytest
 # pytest --cov=app
 # pytest --cov-config=.coveragec --cov=.
+# pytest --cov=app --cov-report=term-missing
 
 
 
--- main-services/service_one/testing/test_app.py
+++ main-services/service_one/testing/test_app.py
@@ -3,7 +3,6 @@ from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
 from app import db
-from application import db, 
 import requests_mock
 
 # pytest
@@ -11,23 +10,23 @@ import requests_mock
 # pytest --cov-config=.coveragec --cov=.
 # pytest --cov=app --cov-report=term-missing
 
-class TestBase(TestCase):
-    def create_app(self):
-        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
-                DEBUG=True,
-        return app    
+# class TestBase(TestCase):
+#     def create_app(self):
+#         app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
+#                 DEBUG=True,
+#         return app    
 
-    def setUp(self):
+#     def setUp(self):
         
-        db.create_all()
+#         db.create_all()
 
-        test_lottery = Lottery(combined_account_string="234567JJK", message_string="Congratulation you won £125", lottery=1)
-        db.session.add(test_lottery)
-        db.session.commit()
+#         test_lottery = Lottery(combined_account_string="234567JJK", message_string="Congratulation you won £125", lottery=1)
+#         db.session.add(test_lottery)
+#         db.session.commit()
     
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+#     def tearDown(self):
+#         db.session.remove()
+#         db.drop_all()
 
 class TestBase(TestCase):
     def create_app(self):
@@ -40,10 +39,10 @@ class TestHome(TestBase):
             m.get("http://service_two:5003/letters").text = ("A")
             response = self.client.get(url_for('home'))
 
-    # def test_view(self):
-    #     response = self.client.get(url_for('home'))
-    #     self.assertIsNotNone(response.status_code, 200)
-    #     self.assertIsNotNone(response.data)
+    def test_view(self):
+        response = self.client.get(url_for('home'))
+        self.assertIsNotNone(response.status_code, 200)
+        self.assertIsNotNone(response.data)
     
     # def test_home_combined(self):
     #     with patch('requests.get') as f:
